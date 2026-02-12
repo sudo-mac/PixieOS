9ef74aef4b9bdea8b3983e699f09590f2589f1c3
PaperMushrooms <PaperMushrooms@users.noreply.github.com>
2026-02-12 10:11:28 -0500

    Removed shells/Learn-Python

--- shells/Learn-Python/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { config.allowUnfree = true; }, }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [ python3 ];
-
-  shellHook = ''
-    echo
-    echo "Time to learn some Python!"
-  '';
-}
