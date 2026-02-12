ab1f09e806fe02f59b27a9764ffe8a534cd403be
PaperMushrooms <PaperMushrooms@users.noreply.github.com>
2026-02-12 04:08:06 -0500

    Removed shells/PoGo-Root/shell.nix and fully converted my shell to a flake

--- shells/PoGo-Root/flake.nix
+++ shells/PoGo-Root/flake.nix
@@ -3,8 +3,28 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShells.default = import ./shell.nix { inherit pkgs; }; });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      scripts = import ./scripts.nix {inherit pkgs;};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [android-tools scrcpy usbmuxd] ++ scripts;
+
+        inputsFrom = [];
+
+        shellHook = ''
+          alias exit="adb kill-server && exit"
+          adb start-server &> /dev/null
+          echo
+          fastfetch --logo android
+          echo
+          echo "Android Rooting and PoGo Spoofing Environment is running!"
+        '';
+      };
+    });
 }
