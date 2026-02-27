f373ac0b97d4b864cddffc044f03129b7128e289
sudo-mac <sudo-mac@users.noreply.github.com>
2026-02-27 09:02:39 -0500

    add nix-darwin

--- flake.nix
+++ flake.nix
@@ -1,30 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     stylix.url = "github:danth/stylix";
-
     nvf = {
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-
     hyprland.url = "github:hyprwm/Hyprland";
     iio-hyprland.url = "github:JeanSchoeller/iio-hyprland";
-
     nixcord.url = "github:kaylorben/nixcord";
-
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -37,22 +31,19 @@
     nixpkgs,
     nvf,
     stylix,
+    nix-darwin,
     ...
   } @ inputs: {
     nixosConfigurations = {
       alienix = nixpkgs.lib.nixosSystem {
-        system = "x86-linux";
-
+        system = "x86_64-linux";
         specialArgs = {inherit inputs;};
-
         modules = [
           ./hosts/alienix/system
           ./modules/system
-
           home-manager.nixosModules.home-manager
           stylix.nixosModules.stylix
           nix-flatpak.nixosModules.nix-flatpak
-
           {
             home-manager = {
               useUserPackages = true;
@@ -61,7 +52,6 @@
                 nvf.homeManagerModules.default
                 nixcord.homeModules.nixcord
               ];
-
               extraSpecialArgs = {inherit inputs;};
               users.dex.imports = [
                 ./hosts/alienix/home
@@ -75,6 +65,18 @@
         specialArgs = {inherit inputs;};
         modules = [./recovery/configuration.nix];
       };
+    };
+
+    darwinConfigurations."Matthews-MacBook-Air" = nix-darwin.lib.darwinSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        {
+          nix.settings.experimental-features = "nix-command flakes";
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          system.stateVersion = 6;
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        }
+      ];
     };
   };
 }
