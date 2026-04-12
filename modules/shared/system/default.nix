0a95dbf2690287db0e895a183d284a453595c8aa
sudo-mac <sudo-mac@users.noreply.github.com>
2026-04-12 12:53:16 -0400

    add shared packages

--- modules/shared/system/default.nix
+++ modules/shared/system/default.nix
@@ -1,6 +1,17 @@
-{
+{pkgs, ...}: {
   imports = [
     ./ssh.nix
     ./stylix.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nh
+    nix-output-monitor
+    bat
+    tree
+    weathr
+    lolcat
+    fastfetch
+    onefetch
   ];
 }
