4145916f5ad814156ba47a3b809dbb98ecf8ca3f
sudo-mac <sudo-mac@users.noreply.github.com>
2026-04-19 09:07:52 -0400

    add shell alias for cp

--- modules/shared/home/shell/aliases.nix
+++ modules/shared/home/shell/aliases.nix
@@ -2,29 +2,30 @@
   pkgs,
   lib,
   ...
-}: {
+}:
+{
   programs = {
-    zsh.shellAliases =
-      {
-        pxie = "cd /etc/nixos/ && git pull";
-        update = "nix flake update --flake .";
-        switch = "nh os switch .";
-        build = "nh os build .";
-        cleanup = "nix-collect-garbage --delete-older-than 15d";
-        cleanup-full = "sudo nix-collect-garbage --delete-older-than 15d";
-        mkrecovery = "nom build .#nixosConfigurations.recovery.config.system.build.isoImage";
+    zsh.shellAliases = {
+      pxie = "cd /etc/nixos/ && git pull";
+      update = "nix flake update --flake .";
+      switch = "nh os switch .";
+      build = "nh os build .";
+      cleanup = "nix-collect-garbage --delete-older-than 15d";
+      cleanup-full = "sudo nix-collect-garbage --delete-older-than 15d";
+      mkrecovery = "nom build .#nixosConfigurations.recovery.config.system.build.isoImage";
 
-        mkdir = "mkdir -p";
-      }
-      // lib.optionalAttrs pkgs.stdenv.isDarwin {
-        switch = "nh darwin switch .#darwin";
-        build = "nh darwin build .#darwin";
-        rsync-flake = "rsync -av alienix:/etc/nixos /etc/nixos";
+      mkdir = "mkdir -p";
+      cp = "cp -r";
+    }
+    // lib.optionalAttrs pkgs.stdenv.isDarwin {
+      switch = "nh darwin switch .#darwin";
+      build = "nh darwin build .#darwin";
+      rsync-flake = "rsync -av alienix:/etc/nixos /etc/nixos";
 
-        ls = "ls --color=always";
-        l = "ls -l --color=always";
-        ll = "ls -la --color=always";
-        tree = "tree -C";
-      };
+      ls = "ls --color=always";
+      l = "ls -l --color=always";
+      ll = "ls -la --color=always";
+      tree = "tree -C";
+    };
   };
 }
